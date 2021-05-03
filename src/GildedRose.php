b6f893cc03b65df32cf95b5b5964799551c17bdd
Odei Alba <odeialba@gmail.com>
2021-05-03 19:19:26 +0200

    Add strict comparison types

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -11,19 +11,19 @@ class GildedRose
         $this->items = $items;
     }
 
-    public function update_quality(): void
+    public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
                         $item->quality = $item->quality - 1;
                     }
                 }
             } else {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->sell_in < 11) {
                             if ($item->quality < 50) {
                                 $item->quality = $item->quality + 1;
@@ -38,15 +38,15 @@ class GildedRose
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
                 $item->sell_in = $item->sell_in - 1;
             }
 
             if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->name !== 'Aged Brie') {
+                    if ($item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
                                 $item->quality = $item->quality - 1;
                             }
                         }
