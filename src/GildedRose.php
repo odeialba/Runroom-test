bc288e836f5b4ac4a2fcf854e720ea8739e1c45c
Odei Alba <odeialba@gmail.com>
2021-05-03 19:48:20 +0200

    Try to split in small conditions

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -7,6 +7,7 @@ class GildedRose
     private CONST NAME_BRIE = 'Aged Brie';
     private CONST NAME_BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
     private CONST NAME_SULFURAS = 'Sulfuras, Hand of Ragnaros';
+
     private array $items;
 
     public function __construct(array $items)
@@ -17,24 +18,18 @@ class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name !== self::NAME_BRIE and $item->name !== self::NAME_BACKSTAGE) {
-                if ($item->quality > 0) {
-                    if ($item->name !== self::NAME_SULFURAS) {
-                        $item->quality--;
-                    }
+            if (! in_array($item->name, [self::NAME_BRIE, self::NAME_BACKSTAGE], true)) {
+                if ($item->quality > 0 && $item->name !== self::NAME_SULFURAS) {
+                    $item->quality--;
                 }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name === self::NAME_BACKSTAGE) {
-                        if ($item->quality < 50) {
-                            if ($item->sell_in < 11) {
-                                $item->quality++;
-                            }
-                            if ($item->sell_in < 6) {
-                                $item->quality++;
-                            }
-                        }
+            } elseif ($item->quality < 50) {
+                $item->quality++;
+                if ($item->name === self::NAME_BACKSTAGE && $item->quality < 50) {
+                    if ($item->sell_in < 11) {
+                        $item->quality++;
+                    }
+                    if ($item->sell_in < 6) {
+                        $item->quality++;
                     }
                 }
             }
@@ -43,22 +38,26 @@ class GildedRose
                 $item->sell_in--;
             }
 
-            if ($item->sell_in < 0) {
-                if ($item->name !== self::NAME_BRIE) {
-                    if ($item->name !== self::NAME_BACKSTAGE) {
-                        if ($item->quality > 0) {
-                            if ($item->name !== self::NAME_SULFURAS) {
-                                $item->quality--;
-                            }
-                        }
-                    } else {
-                        $item->quality = 0;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality++;
-                    }
+            if ($item->sell_in >= 0) {
+                continue;
+            }
+
+            if ($item->name === self::NAME_BACKSTAGE) {
+                $item->quality = 0;
+                continue;
+            }
+
+            if ($item->name === self::NAME_BRIE) {
+                if ($item->quality < 50) {
+                    $item->quality++;
                 }
+
+                continue;
+            }
+
+            if ($item->quality > 0 && $item->name !== self::NAME_SULFURAS) {
+                $item->quality--;
+                continue;
             }
         }
     }
